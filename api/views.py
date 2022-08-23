f18f368f9f625390f7c800b24846690105e11701
AleksandrB00 <babaev_aleksandr@icloud.com>
2022-08-23 11:54:02 +0200

    confirm_email

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .serializers import *
 from rest_framework import viewsets, permissions, generics
 from rest_framework.response import Response
+from django.core.mail import send_mail
 
 
 class SignUpView(generics.GenericAPIView):
@@ -10,8 +11,9 @@ class SignUpView(generics.GenericAPIView):
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        
         return Response({
             'message' : 'Пользователь успешно создан'
         })
@@ -19,6 +21,9 @@ class SignUpView(generics.GenericAPIView):
 class ConfirmEmailView(generics.GenericAPIView):
     serializer_class = ConfirmEmailSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+         send_mail('Код подтверждения', f'здравствуйте {request.user.username}, ваш код потверждения - {request.user.profile.conf_code}', [request.user.email])
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
